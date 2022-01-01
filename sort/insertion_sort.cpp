d311abaac0aa62a2d7a7eb5108ca0e24dda3fc67
MK-nn <okuken99@gmail.com>
2022-01-01 10:30:40 +0900

    add trace function

--- sort/insertion_sort.cpp
+++ sort/insertion_sort.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
 
+void trace(int array[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    if (i > 0)
+    {
+      std::cout << " " << std::endl;
+    }
+    std::cout << array[i] << std::endl;
+  }
+  std::cout << std::endl;
+}
+
 int main()
 {
 }
