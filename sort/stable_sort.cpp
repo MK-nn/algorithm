44bef26fc8fbd3b1aa15074fb26281d09001837b
MK-nn <okuken99@gmail.com>
2022-01-03 14:03:17 +0900

    add bubble sort

--- sort/stable_sort.cpp
+++ sort/stable_sort.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
-struct Cart
+typedef struct Cart
 {
   char suit;
   int value;
-};
+} CARD;
+
+void bubbleSort(CARD array[], int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = n - 1; j >= i + 1; j--)
+    {
+      if (array[j].value < array[j - 1].value)
+      {
+        std::swap(array[j], array[j - 1]);
+      }
+    }
+  }
+}
