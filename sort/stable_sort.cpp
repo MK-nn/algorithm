f7ad33af76790bdeb7d623f4248253a7e2f2c25b
MK-nn <okuken99@gmail.com>
2022-01-03 14:22:42 +0900

    update main function

--- sort/stable_sort.cpp
+++ sort/stable_sort.cpp
@@ -56,3 +56,44 @@ bool isStable(CARD array_1[], CARD array_2[], int n)
   }
   return true;
 }
+
+int main()
+{
+  int n;
+  std::cin >> n;
+  CARD array_1[n];
+  CARD array_2[n];
+
+  for (int i = 0; i < n; i++)
+  {
+    std::cin >> array_1[i].suit >> array_1[i].value;
+  }
+
+  for (int i = 0; i < n; i++)
+  {
+    array_2[i] = array_1[i];
+  }
+
+  std::cout << "Before sort: ";
+  printCard(array_1, n);
+
+  bubbleSort(array_1, n);
+  selectionSort(array_2, n);
+
+  std::cout << "After bubble sort: ";
+  printCard(array_1, n);
+
+  std::cout << "After selection sort: ";
+  printCard(array_2, n);
+
+  if (isStable(array_1, array_2, n))
+  {
+    std::cout << "Stable" << std::endl;
+  }
+  else
+  {
+    std::cout << "Not Stable" << std::endl;
+  }
+
+  return 0;
+}
