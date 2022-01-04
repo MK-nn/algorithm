775a9801350b867cd74bdee2c51838eaddeead15
MK-nn <okuken99@gmail.com>
2022-01-04 23:03:37 +0900

    fix output

--- data_structure/c_stack.cpp
+++ data_structure/c_stack.cpp
@@ -12,7 +12,7 @@ int main()
   S.push(7);
   std::cout << "stack に 1 を積む" << std::endl;
   S.push(1);
-  std::cout << "stack の size を出力する" << std::endl;
+  std::cout << "stack の size を出力する -> ";
   std::cout << S.size() << std::endl;
 
   std::cout << "pop する" << std::endl;
