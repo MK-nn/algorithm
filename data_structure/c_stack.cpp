cfe05cb9e03b2c78692fabfe10f25ad5c3cf9032
MK-nn <okuken99@gmail.com>
2022-01-04 23:06:46 +0900

    refactoring

--- data_structure/c_stack.cpp
+++ data_structure/c_stack.cpp
@@ -8,28 +8,34 @@ int main()
 
   std::cout << "stack に 3 を積む" << std::endl;
   S.push(3);
-  std::cout << "current stack top -> " << S.top() << std::endl;
+  std::cout << "current top of stack -> " << S.top() << std::endl;
+
   std::cout << "stack に 7 を積む" << std::endl;
   S.push(7);
-  std::cout << "current stack top -> " << S.top() << std::endl;
+  std::cout << "current top of stack -> " << S.top() << std::endl;
+
   std::cout << "stack に 1 を積む" << std::endl;
   S.push(1);
-  std::cout << "current stack top -> " << S.top() << std::endl;
+  std::cout << "current top of stack -> " << S.top() << std::endl;
+
   std::cout << "stack の size を出力する -> ";
   std::cout << S.size() << std::endl;
 
   std::cout << "pop する" << std::endl;
   S.pop();
-  std::cout << "current stack top -> " << S.top() << std::endl;
+  std::cout << "current top of stack -> " << S.top() << std::endl;
+
   std::cout << "pop する" << std::endl;
   S.pop();
-  std::cout << "current stack top -> " << S.top() << std::endl;
+  std::cout << "current top of stack -> " << S.top() << std::endl;
+
   std::cout << "stack に 5 を積む" << std::endl;
   S.push(5);
-  std::cout << "current stack top -> " << S.top() << std::endl;
+  std::cout << "current top of stack -> " << S.top() << std::endl;
+
   std::cout << "pop する" << std::endl;
   S.pop();
-  std::cout << "current stack top -> " << S.top() << std::endl;
+  std::cout << "current top of stack -> " << S.top() << std::endl;
 
   return 0;
 }
