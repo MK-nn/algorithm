3e697fc0884ef6db7e426da5b3114308792e9881
MK-nn <okuken99@gmail.com>
2022-01-05 13:40:19 +0900

    update main function

--- data_structure/c_queue.cpp
+++ data_structure/c_queue.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <queue>
+#include <string>
 // c++ の標準ライブラリの queue を使用する
 
 int main()
 {
+  std::queue<std::string> Q;
+
+  Q.push("red");
+  Q.push("yellow");
+  Q.push("yellow");
+  Q.push("blue");
+
+  Q.pop();
+  Q.pop();
+  Q.pop();
+  Q.push("green");
+  Q.pop();
   return 0;
 }
