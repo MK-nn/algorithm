0bf24d44e7c6b295e2662a4603c841959fc7246a
MK-nn <okuken99@gmail.com>
2022-01-06 21:25:42 +0900

    update main function

--- data_structure/c_vector.cpp
+++ data_structure/c_vector.cpp
@@ -13,5 +13,20 @@ void printVector(std::vector<double> V)
 
 int main()
 {
+  std::vector<double> V;
+
+  V.push_back(0.1);
+  V.push_back(0.2);
+  V.push_back(0.3);
+  V[2] = 0.4;
+  printVector(V);
+
+  V.insert(V.begin() + 2, 0.8);
+  printVector(V);
+  V.erase(V.begin() + 1);
+  printVector(V);
+
+  V.push_back(0.9);
+  printVector(V);
   return 0;
 }
