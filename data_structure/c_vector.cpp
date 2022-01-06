ebddf222022e29c4a4841082d1c8e44a08c13d4e
MK-nn <okuken99@gmail.com>
2022-01-06 21:21:19 +0900

    add print function

--- data_structure/c_vector.cpp
+++ data_structure/c_vector.cpp
@@ -2,6 +2,15 @@
 #include <vector>
 // c++ の標準ライブラリの vector を使用する
 
+void printVector(std::vector<double> V)
+{
+  for (int i = 0; i < V.size(); i++)
+  {
+    std::cout << V[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
 int main()
 {
   return 0;
