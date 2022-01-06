7f7535b24234cb62642cb00f61dcd1e4f9cfa5ca
MK-nn <okuken99@gmail.com>
2022-01-06 21:44:32 +0900

    update main function

--- data_structure/c_list.cpp
+++ data_structure/c_list.cpp
@@ -4,5 +4,20 @@
 
 int main()
 {
+  std::list<char> L;
+
+  L.push_front('b');
+  L.push_back('c');
+  L.push_front('a');
+
+  std::cout << L.front();
+  std::cout << L.back();
+
+  L.pop_front();
+  L.push_back('d');
+
+  std::cout << L.front();
+  std::cout << L.back() << std::endl;
+
   return 0;
 }
