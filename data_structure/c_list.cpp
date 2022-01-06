5d97bfd060cc7141b30beb41aae46727fdec4580
MK-nn <okuken99@gmail.com>
2022-01-06 21:51:35 +0900

    fix output

--- data_structure/c_list.cpp
+++ data_structure/c_list.cpp
@@ -6,14 +6,22 @@ int main()
 {
   std::list<char> L;
 
+  std::cout << "push_front b" << std::endl;
   L.push_front('b');
+
+  std::cout << "push_back c" << std::endl;
   L.push_back('c');
+
+  std::cout << "push_front a" << std::endl;
   L.push_front('a');
 
   std::cout << L.front();
   std::cout << L.back();
 
+  std::cout << "pop_front" << std::endl;
   L.pop_front();
+
+  std::cout << "push_back d" << std::endl;
   L.push_back('d');
 
   std::cout << L.front();
