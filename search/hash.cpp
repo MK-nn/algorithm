0e7df78c9d230404d1914aafb92f163ca7089db2
MK-nn <okuken99@gmail.com>
2022-01-12 23:10:07 +0900

    create insert linked list

--- search/hash.cpp
+++ search/hash.cpp
@@ -7,6 +7,14 @@ typedef struct list
   struct list *next;
 } LIST;
 
+void insertLinkedList(LIST **head, int data)
+{
+  LIST *x = (LIST *)malloc(sizeof(LIST));
+  x->data = data;
+  x->next = *head;
+  *head = x;
+}
+
 int main()
 {
   return 0;
