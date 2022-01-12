f7c5694b915529d177c4653ed2b31727838e41b2
MK-nn <okuken99@gmail.com>
2022-01-12 23:12:30 +0900

    update main function

--- search/hash.cpp
+++ search/hash.cpp
@@ -65,5 +65,69 @@ int hash(int data, int table_size)
 
 int main()
 {
-  return 0;
+  std::cout << "hash table の大きさを入力してください" << std::endl;
+  int table_size;
+  std::cin >> table_size;
+
+  // initialize
+  LIST *hash_table[table_size];
+  for (int i = 0; i < table_size; i++)
+  {
+    hash_table[i] = NULL;
+  }
+
+  while (true)
+  {
+    std::cout << "行う操作を選んでください" << std::endl;
+    std::cout << "1.insert 2.delete 3.search 4.exit" << std::endl;
+
+    int n;
+    std::cin >> n;
+    if (n == 1)
+    {
+      std::cout << "挿入する数字を選択してください" << std::endl;
+      int data;
+      std::cin >> data;
+
+      // chainedHashInsert
+      int i = hash(data, table_size);
+      insertLinkedList(&hash_table[i], data);
+    }
+    else if (n == 2)
+    {
+      std::cout << "削除する数字を選択してください" << std::endl;
+      int data;
+      std::cin >> data;
+
+      // chainedHastDelete
+      int i = hash(data, table_size);
+      deleteLinkedList(&hash_table[i], data);
+    }
+    else if (n == 3)
+    {
+      std::cout << "検索する数字を選択してください" << std::endl;
+      int data;
+      std::cin >> data;
+
+      // chainedHashSearch
+      int i = hash(data, table_size);
+      if (searchLinkedList(hash_table[i], data))
+      {
+        std::cout << "数字が見つかりました" << std::endl;
+      }
+      else
+      {
+        std::cout << "数字が見つかりませんでした" << std::endl;
+      }
+    }
+    else if (n == 4)
+    {
+      return 0;
+    }
+    else
+    {
+      std::cout << "1~4の数字を選んでください" << std::endl;
+      std::cout << std::endl;
+    }
+  }
 }
