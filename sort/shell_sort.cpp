c095a18906135578f066cb40b0eb728e340448ce
MK-nn <okuken99@gmail.com>
2022-01-30 09:58:28 +0900

    add shell sort

--- sort/shell_sort.cpp
+++ sort/shell_sort.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+// TODO:整理する
+
 long long cnt;
+int A[1000000];
+int n;
+std::vector<int> G;
 
 void insertionSort(int A[], int n, int g)
 
@@ -14,4 +24,44 @@ void insertionSort(int A[], int n, int g)
     }
     A[j + g] = v;
   }
+}
+
+void shellSort(int A[], int n)
+{
+  for (int h = 1;;)
+  {
+    if (h > n)
+      break;
+    G.push_back(h);
+    h = 3 * h + 1;
+  }
+
+  for (int i = G.size() - 1; i >= 0; i--)
+  {
+    insertionSort(A, n, G[i]);
+  }
+}
+
+int main()
+{
+  std::cin >> n;
+  for (int i = 0; i < n; i++)
+    scanf("%d\n", A[i]);
+  cnt = 0;
+
+  shellSort(A, n);
+
+  std::cout << G.size() << std::endl;
+  for (int i = G.size() - 1; i >= 0; i--)
+  {
+    printf("%d\n", G[i]);
+    if (i)
+      printf(" ");
+  }
+  std::cout << std::endl;
+  printf("%d\n", cnt);
+  for (int i = 0; i < n; i++)
+    printf("%d\n", A[i]);
+
+  return 0;
 }
